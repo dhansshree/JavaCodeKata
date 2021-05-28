c6473425d8bdc992f37ddc85e2718c362d24ac76
Dhanya Sridharan <dhanya.sridharan@cognizant.com>
2021-05-28 10:17:39 +0530

    Latest code

--- JavaCodeKata/src/com/company/Main.java
+++ JavaCodeKata/src/com/company/Main.java
@@ -2,13 +2,51 @@ package com.company;
 
 import com.company.basics.*;
 
+import java.lang.reflect.Method;
 import java.util.HashMap;
+
+
+class Printer
+{
+    //Write your code here
+    public  static <T> void printArray(T[] input)
+    {
+        for(int i=0; i< input.length;i++)
+        {
+            System.out.println(input[i]);
+        }
+    }
+
+}
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        UtilityIntro.HashSet();
+
+
         //UtilityIntro.StudentDBList();
-        UtilityIntro.Print();
+        //UtilityIntro.Print();
+
+     /*   Printer myPrinter = new Printer();
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
+
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+
+            if(name.equals("printArray"))
+                count++;
+        }
+
+        if(count > 1)System.out.println("Method overloading is not allowed!");*/
+
+
+
+
     }
 }
--- JavaCodeKata/src/com/company/basics/UtilityIntro.java
+++ JavaCodeKata/src/com/company/basics/UtilityIntro.java
@@ -1,9 +1,74 @@
 package com.company.basics;
 
+import java.lang.reflect.Array;
+import java.net.Inet4Address;
 import java.text.NumberFormat;
+import java.time.DayOfWeek;
+import java.time.format.TextStyle;
 import java.util.*;
+import java.util.regex.Pattern;
 
 public class UtilityIntro {
+
+    public static void printDay()
+    {
+        Scanner in = new Scanner(System.in);
+        String month = in.next();
+        String day = in.next();
+        String year = in.next();
+
+        //System.out.println(getDay(day, month, year));
+
+    }
+
+    public static void getDay(String day, String month, String year) {
+        /*Calendar calendar = new GregorianCalendar(Integer.parseInt(year), Integer.parseInt(month), Integer.parseInt(day));
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+        String dayWeekText = new SimpleDateFormat("EEEE").format(calendar.getD);
+        System.out.println("Day of week in text:"+dayWeekText);*/
+    }
+
+    public static void HashSet()
+    {
+        Set<Integer> integerHashSet = new HashSet<Integer>();
+        Scanner scanner = new Scanner(System.in);
+        Integer input = scanner.nextInt();
+        for(int i=1;i<=input;i++)
+        {
+            Integer id = scanner.nextInt();
+            if(!integerHashSet.contains(id))
+                integerHashSet.add(id);
+        }
+        System.out.println(integerHashSet.size());
+    }
+
+
+    public static void PatternValidate()
+    {
+        Set<Integer> integerHashSet = new HashSet<Integer>();
+        Scanner scanner = new Scanner(System.in);
+        Integer input = scanner.nextInt();
+
+        ArrayList<String> output = new ArrayList<>();
+        for(int i=1;i<=input;i++)
+        {
+            String data = scanner.next();
+            try
+            {
+                Pattern.compile(data);
+                output.add("Valid");
+            }
+            catch (Exception ex)
+            {
+                output.add("Invalid");
+            }
+
+        }
+        for (String elem:output
+             ) {
+            System.out.println(elem);
+        }
+    }
 
     public static void StudentDBList()
     {
